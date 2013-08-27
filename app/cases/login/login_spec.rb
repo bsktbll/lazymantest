4d68fe440995f6fefdcaeed7e7fe388e98caa522
bsktbll <bsktbll@gmail.com>
2013-08-27 22:45:09 +0800

    data drive

--- app/cases/login/login_spec.rb
+++ app/cases/login/login_spec.rb
@@ -1,12 +1,17 @@
 #encoding:utf-8
 require File.expand_path 'app/cases/spec_helper'
 describe 'login to weixinwork',:login do
-	
+	before :all do 
+		@logindata = test_data "login"
+		@username=@logindata["user1"]
+		@userpassword=@logindata["password"]
+		p 
+	end
 	it 'should login to weixinwork' do 
 		puts $navi.public_methods
 		login_page=$navi.goto_login_page    
 		
-		login_page.login 't1@t20.com','111111'
+		login_page.login @username,@userpassword
 		
 	end
 end
